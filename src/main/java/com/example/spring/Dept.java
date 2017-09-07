903b4cd8b36d7cf66b1c037f3965cacf5e6d7fc6
java <java@bigdata>
2017-09-07 11:11:59 +0900

    lomok apply

--- src/main/java/com/example/spring/Dept.java
+++ src/main/java/com/example/spring/Dept.java
@@ -1,26 +1,37 @@
 package com.example.spring;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString   // tostring 해주는것이다. 
+@Getter
+@Setter    // 문복을깔면 이렇게 된다.~ toString getter setter 다만들어진다
+@AllArgsConstructor // 필드를 생성자를 만들어준다.
+@NoArgsConstructor // 디폴드 생성자가 만들어진다.
 public class Dept {
 	int deptno;
 	String dname;
 	String loc;
-	public int getDeptno() {
-		return deptno;
-	}
-	public void setDeptno(int deptno) {
-		this.deptno = deptno;
-	}
-	public String getDname() {
-		return dname;
-	}
-	public void setDname(String dname) {
-		this.dname = dname;
-	}
-	public String getLoc() {
-		return loc;
-	}
-	public void setLoc(String loc) {
-		this.loc = loc;
-	} 
+//	public int getDeptno() {
+//		return deptno;
+//	}
+//	public void setDeptno(int deptno) {
+//		this.deptno = deptno;
+//	}
+//	public String getDname() {
+//		return dname;
+//	}
+//	public void setDname(String dname) {
+//		this.dname = dname;
+//	}
+//	public String getLoc() {
+//		return loc;
+//	}
+//	public void setLoc(String loc) {
+//		this.loc = loc;
+//	} 
 
 }
--- src/main/java/com/example/spring/DeptExample.java
+++ src/main/java/com/example/spring/DeptExample.java
@@ -9,7 +9,7 @@ public class DeptExample {
 		dept.setDname("개발부");
 		dept.setLoc("서울");
 		
-		System.out.println(dept); // 정의해주면 이렇게 쓸수있다. toString이용하여서...
+		System.out.println(dept); // 정의해주면 이렇게 쓸수있다. toString이용하여서... 이게 반복해서 소스 재정의하는게 힘들다
 		System.out.println(dept.getDeptno()+", " + dept.getDname()+"," + dept.getLoc() );
 	}
 
